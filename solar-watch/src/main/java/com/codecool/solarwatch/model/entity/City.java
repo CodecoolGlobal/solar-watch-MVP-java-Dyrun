9a8a800b49d60c4649c9d4778003c859e51edb01
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-03-15 23:59:04 +0100

    Refactor City entity

---
+++ solar-watch/src/main/java/com/codecool/solarwatch/model/entity/City.java
@@ -0,0 +1,24 @@
+package com.codecool.solarwatch.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "cities")
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private double latitude;
+    private double longitude;
+    private String state;
+    private String country;
+
+}
