42c16530634d91cf58aa951f41c150f476dd7c7a
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-03-16 00:08:10 +0100

    Refactor SunriseSunset

---
+++ solar-watch/src/main/java/com/codecool/solarwatch/model/entity/SunriseSunset.java
@@ -0,0 +1,27 @@
+package com.codecool.solarwatch.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "sunrise_sunset")
+public class SunriseSunset {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @ManyToOne
+    @JoinColumn(name = "city_id")
+    private City city;
+    private String sunrise;
+    private String sunset;
+    private LocalDate date;
+
+}
