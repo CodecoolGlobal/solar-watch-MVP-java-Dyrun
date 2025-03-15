548b241341659c557be0c85cb41a79e5bf10c06d
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-03-16 00:06:07 +0100

    Create Role entity

---
+++ solar-watch/src/main/java/com/codecool/solarwatch/model/entity/Role.java
@@ -0,0 +1,21 @@
+package com.codecool.solarwatch.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+}
