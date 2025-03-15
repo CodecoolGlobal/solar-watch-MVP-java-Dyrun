02f32adb8aaa918cbb84ee7405458ef714f1b371
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-03-15 16:01:13 +0100

    Extend GeocodingReport

--- solar-watch/src/main/java/com/codecool/solarwatch/model/GeocodingReport.java
+++ solar-watch/src/main/java/com/codecool/solarwatch/model/GeocodingReport.java
@@ -3,5 +3,5 @@ package com.codecool.solarwatch.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record GeocodingReport(double lat, double lon) {
+public record GeocodingReport(double lat, double lon, String name, String country, String state) {
 }
