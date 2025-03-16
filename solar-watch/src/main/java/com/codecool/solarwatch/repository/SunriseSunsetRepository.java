4d2b0891a9ca8268acb4ca49c6fc8e76dd334260
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-03-16 13:44:41 +0100

    Add findByCity

--- solar-watch/src/main/java/com/codecool/solarwatch/repository/SunriseSunsetRepository.java
+++ solar-watch/src/main/java/com/codecool/solarwatch/repository/SunriseSunsetRepository.java
@@ -1,12 +1,15 @@
 package com.codecool.solarwatch.repository;
 
+import com.codecool.solarwatch.model.dto.SunriseSunsetResponse;
 import com.codecool.solarwatch.model.entity.City;
 import com.codecool.solarwatch.model.entity.SunriseSunset;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface SunriseSunsetRepository extends JpaRepository<SunriseSunset, Long> {
     Optional<SunriseSunset> findByCityAndDate(City city, LocalDate date);
+    List<SunriseSunsetResponse> findByCity(City city);
 }
