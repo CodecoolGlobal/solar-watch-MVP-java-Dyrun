1aed97617dc50703483d5c8d5bcb5a5703740754
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-03-16 13:43:56 +0100

    Add endpoints for exceptions

--- solar-watch/src/main/java/com/codecool/solarwatch/controller/SolarWatchControllerAdvice.java
+++ solar-watch/src/main/java/com/codecool/solarwatch/controller/SolarWatchControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.codecool.solarwatch.controller;
 
+import org.postgresql.util.PSQLException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.time.format.DateTimeParseException;
+import java.util.NoSuchElementException;
 
 @ControllerAdvice
 public class SolarWatchControllerAdvice {
@@ -22,6 +24,20 @@ public class SolarWatchControllerAdvice {
     @ExceptionHandler(NullPointerException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String nullPointerExceptionHandler(NullPointerException ex) {
+        return ex.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String noSuchElementException(NoSuchElementException ex) {
+        return ex.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(PSQLException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String psqlException(PSQLException ex) {
         return ex.getMessage();
     }
 }
