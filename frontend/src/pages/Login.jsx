e2c2641d7ca90be4a383794113f9365978d35e09
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-05-10 23:20:03 +0200

    Refactor Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,74 +1,43 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import Error from "../components/Error.jsx";
+import LoginForm from "../components/forms/LoginForm.jsx";
+import {useAuth} from "../components/AuthProvider.jsx";
 
-function Login({ setAuth }) {
-    const [credentials, setCredentials] = useState({ username: "", password: "" });
+function Login() {
     const navigate = useNavigate();
-    const [error, setError] = useState(null);
+    const [error, setError] = useState(false);
+    const [errorMsg, setErrorMsg] = useState("");
+    const {login} = useAuth();
 
-    const handleChange = (e) => {
-        setCredentials({ ...credentials, [e.target.name]: e.target.value });
-    };
-
-    const handleSubmit = async (e) => {
+    async function handleSubmit(e, credentials) {
         e.preventDefault();
-        const response = await fetch("/api/user/signin", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(credentials),
-        });
-        const data = await response.json();
-        if (response.ok) {
-            localStorage.setItem("jwt", data.jwt);
-            setAuth(true);
-            navigate("/solar-watch");
-        } else {
-            setError("Invalid username or password");
+        try {
+            const response = await fetch("/api/user/login", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(credentials),
+            });
+            if (response.ok) {
+                const data = await response.json();
+                login(data);
+                setError(false);
+                navigate("/solar-watch");
+            } else {
+                setErrorMsg("Invalid username or password");
+                setError(true);
+            }
+
+        } catch (error) {
+            setErrorMsg(error.message);
+            setError(true);
         }
-    };
+    }
 
     return (
-        <div className="min-h-screen w-full flex items-center justify-center p-4">
-            <form onSubmit={handleSubmit} className="w-full max-w-xs">
-                <fieldset className="fieldset bg-base-200 border border-base-300 p-4 rounded-box w-full">
-                    <legend className="fieldset-legend">Login</legend>
-
-                    <div className="space-y-4">
-                        <div>
-                            <label className="fieldset-label">Username</label>
-                            <input
-                                type="text"
-                                name="username"
-                                className="input w-full"
-                                placeholder="Username"
-                                onChange={handleChange}
-                            />
-                        </div>
-
-                        <div>
-                            <label className="fieldset-label">Password</label>
-                            <input
-                                type="password"
-                                name="password"
-                                className="input w-full"
-                                placeholder="Password"
-                                onChange={handleChange}
-                            />
-                        </div>
-                        {error && (
-                            <div className="alert alert-error mt-4">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                </svg>
-                                <span>{error}</span>
-                            </div>
-                        )}
-                        <button type="submit" className="btn btn-neutral w-full mt-4">
-                            Login
-                        </button>
-                    </div>
-                </fieldset>
-            </form>
+        <div className="min-h-screen w-full flex flex-col items-center justify-center p-4">
+            <LoginForm onSubmit={handleSubmit}/>
+            <Error errorMsg={errorMsg} display={error}/>
         </div>
     );
 }
