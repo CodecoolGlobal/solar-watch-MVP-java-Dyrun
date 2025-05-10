2d026df71c81a4a0fe63c7c7767bd6f1ce292add
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-05-10 23:11:02 +0200

    Refactor CityController

--- backend/src/main/java/com/codecool/solarwatch/controller/CityController.java
+++ backend/src/main/java/com/codecool/solarwatch/controller/CityController.java
@@ -2,12 +2,11 @@ package com.codecool.solarwatch.controller;
 
 import com.codecool.solarwatch.model.entity.City;
 import com.codecool.solarwatch.service.CityService;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/city")
@@ -19,40 +18,37 @@ public class CityController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<City> createCity(@RequestBody City city) {
-        return ResponseEntity.ok(cityService.saveCity(city));
+    public City createCity(@RequestBody City city) {
+        return cityService.saveCity(city);
     }
 
-    @GetMapping
-    public ResponseEntity<List<City>> getAllCities() {
-        return ResponseEntity.ok(cityService.getAllCities());
+    @GetMapping("/all")
+    public List<City> getAllCities() {
+        return cityService.getAllCities();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<City> getCityById(@PathVariable Long id) {
-        Optional<City> city = cityService.getCityById(id);
-        return city.map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    public City getCityById(@PathVariable Long id) {
+        return cityService.getCityById(id);
     }
 
-    @GetMapping("/name/{name}")
-    public ResponseEntity<City> getCityByName(@PathVariable String name) {
-        Optional<City> city = cityService.getCityByName(name);
-        return city.map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    @GetMapping
+    public City getCityByName(@RequestParam String name) {
+        return cityService.getCityByName(name);
     }
 
     @PutMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<City> updateCity(@PathVariable Long id, @RequestBody City updatedCity) {
-        return ResponseEntity.ok(cityService.updateCity(id, updatedCity));
+    public City updateCity(@PathVariable Long id, @RequestBody City updatedCity) {
+        return cityService.updateCity(id, updatedCity);
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Void> deleteCity(@PathVariable Long id) {
+    public void deleteCity(@PathVariable Long id) {
         cityService.deleteCity(id);
-        return ResponseEntity.noContent().build();
     }
 }
