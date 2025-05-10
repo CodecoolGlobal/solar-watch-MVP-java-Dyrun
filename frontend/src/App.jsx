b5cafa36dc6f17cbf90bbdd722529f74b206584c
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-05-10 23:18:46 +0200

    Refactor App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,21 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { useState, useEffect } from "react";
+import {Route, Routes} from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import SolarWatch from "./pages/SolarWatch";
-import Navbar from "./components/Navbar.jsx";
 import "./App.css";
+import Layout from "./components/Layout.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 function App() {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-    useEffect(() => {
-        const token = localStorage.getItem("jwt");
-        setIsAuthenticated(!!token);
-    }, []);
-
-    function handleLogout() {
-        localStorage.removeItem("jwt");
-        setIsAuthenticated(false);
-    }
 
     return (
-        <Router>
-            <Navbar isAuthenticated={isAuthenticated} onLogout={handleLogout} />
-            <Routes>
-                <Route path="/login" element={<Login setAuth={setIsAuthenticated} />} />
-                <Route path="/registration" element={<Register />} />
-                <Route
-                    path="/solar-watch"
-                    element={isAuthenticated ? <SolarWatch /> : <Navigate to="/login" />}
-                />
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path="/" element={<Layout/>}>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/registration" element={<Register/>}/>
+                <Route path="/solar-watch" element={<ProtectedRoute><SolarWatch/></ProtectedRoute>}/>
+            </Route>
+        </Routes>
     );
 }
 
