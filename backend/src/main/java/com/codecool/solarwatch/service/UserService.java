26c6143b27b1f781def2acb730bc342246e6277a
Marcell Nagy <43303974+Dyrun@users.noreply.github.com>
2025-05-10 23:16:37 +0200

    Refactor UserService

--- backend/src/main/java/com/codecool/solarwatch/service/UserService.java
+++ backend/src/main/java/com/codecool/solarwatch/service/UserService.java
@@ -1,25 +1,41 @@
 package com.codecool.solarwatch.service;
 
+import com.codecool.solarwatch.model.dto.user.JwtResponse;
+import com.codecool.solarwatch.model.dto.user.UserRequest;
+import com.codecool.solarwatch.model.dto.user.UserUpdateResponse;
 import com.codecool.solarwatch.model.entity.Member;
 import com.codecool.solarwatch.model.entity.Role;
 import com.codecool.solarwatch.model.entity.RoleType;
 import com.codecool.solarwatch.repository.MemberRepository;
 import com.codecool.solarwatch.repository.RoleRepository;
+import com.codecool.solarwatch.security.jwt.JwtUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
-import java.util.logging.Logger;
 
 @Service
 public class UserService {
     private final MemberRepository memberRepository;
     private final RoleRepository roleRepository;
-    private final Logger logger = Logger.getLogger(UserService.class.getName());
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtils jwtUtils;
+    private final Logger logger = LoggerFactory.getLogger(UserService.class);
 
-    public UserService(MemberRepository memberRepository, RoleRepository roleRepository) {
+    public UserService(MemberRepository memberRepository, RoleRepository roleRepository, AuthenticationManager authenticationManager, JwtUtils jwtUtils) {
         this.memberRepository = memberRepository;
         this.roleRepository = roleRepository;
+        this.authenticationManager = authenticationManager;
+        this.jwtUtils = jwtUtils;
     }
 
     public void createUser(String name, String password) {
@@ -31,13 +47,28 @@ public class UserService {
         memberRepository.save(user);
     }
 
-    public Member addAdminToUser(String username) {
-        logger.info("Adding admin to user: " + username);
+    public UserUpdateResponse addAdminToUser(String username) {
+        logger.info("Adding admin to user: {}", username);
         Member user = memberRepository.findByName(username).orElseThrow();
         Role roleAdmin = roleRepository.findByRoleType(RoleType.ROLE_ADMIN).orElseThrow();
         Set<Role> updatedRoles = new HashSet<>(user.getRoles());
         updatedRoles.add(roleAdmin);
         user.setRoles(updatedRoles);
-        return memberRepository.save(user);
+        memberRepository.save(user);
+        return new UserUpdateResponse(user.getName(), user.getRoles());
+    }
+
+    public JwtResponse loginUser(UserRequest loginRequest) {
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.username(), loginRequest.password()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        User userDetails = (User) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority)
+                .toList();
+
+        return new JwtResponse(jwt, userDetails.getUsername(), roles);
     }
 }
